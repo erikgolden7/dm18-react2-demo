4d30bef692f651e198073d7c37db72899afd2eb4
Erik Golden <erikgolden7@gmail.com>
2018-09-10 09:36:55 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import PeopleList from './components/PeopleList';
+import PickedList from './components/PickedList';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      people: ['John', 'Bob', 'Harry', 'Lina', 'Stacy'],
+      picked: []
+    };
+  }
+
+  handleConfirm = person => {
+    const peopleCopy = this.state.people.slice();
+    const index = peopleCopy.indexOf(person);
+    peopleCopy.splice(index, 1);
+
+    const pickedCopy = this.state.picked.slice();
+    pickedCopy.push(person);
+
+    this.setState({ people: peopleCopy, picked: pickedCopy });
+  };
+
+  render() {
+    const { people, picked } = this.state;
+
+    let peopleList = people.map((element, index, arr) => {
+      return (
+        <PeopleList
+          key={index}
+          name={element}
+          confirmMethod={this.handleConfirm}
+        />
+      );
+    });
+
+    let pickedList = picked.map((element, index, arr) => {
+      return <PickedList key={index} name={element} />;
+    });
+
+    return (
+      <div>
+        <h3> General List </h3>
+        {peopleList}
+        <PickedList name="John" confirmMethod={this.handleConfirm} />
+        <PickedList name="Bob" confirmMethod={this.handleConfirm} />
+        <PickedList name="Harry" confirmMethod={this.handleConfirm} />
+        <PickedList name="Lina" confirmMethod={this.handleConfirm} />
+        <PickedList name="Stacy" confirmMethod={this.handleConfirm} />
+        <h3> Picked List </h3>
+        {pickedList}
+      </div>
+    );
+  }
+}
+
+export default App;
